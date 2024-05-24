2077ba284dcaf0b16e8d2a8d8f92b1c7e5575d72
Dmitry Rychkov <dmitrii@rychkov.pro>
2024-05-24 14:28:26 +0500

    secondary commit

---
+++ bese.js
@@ -0,0 +1,9 @@
+
+const div = document.createElement('div');
+const header = document.createElement('h1');
+header.innerText = 'Learn JS'
+
+div.appendChild(header)
+document.body.appendChild(div)
+console.log(div)
+console.log(header)
